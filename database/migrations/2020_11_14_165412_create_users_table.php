a642bd883f8d11adec59dc875f9a281ff32acedb
Lukas Stock <71986653+lukasstock@users.noreply.github.com>
2021-01-22 11:08:51 +0100

    Bugfix

--- database/migrations/2020_11_14_165412_create_users_table.php
+++ database/migrations/2020_11_14_165412_create_users_table.php
@@ -15,7 +15,6 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique()->change();
             $table->string('isAdmin');
             $table->timestamps();
         });
